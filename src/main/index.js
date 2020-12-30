0ea82ceb6b66b29d3bd9f6db8a6f13e672561d99
litterwang <1494601749@qq.com>
2020-12-30 15:52:36 +0800

    first commit

---
+++ src/main/index.js
@@ -0,0 +1,51 @@
+import { app, BrowserWindow } from 'electron'
+
+/**
+ * Set `__static` path to static files in production
+ * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
+ */
+if (process.env.NODE_ENV !== 'development') {
+  global.__static = require('path')
+    .join(__dirname, '/static')
+    .replace(/\\/g, '\\\\')
+}
+
+let mainWindow
+const winURL =
+  process.env.NODE_ENV === 'development'
+    ? `http://localhost:9080`
+    : `file://${__dirname}/index.html`
+
+function createWindow() {
+  /**
+   * Initial window options
+   */
+ 
+  mainWindow = new BrowserWindow({
+    height: 670, //窗口高度
+    width: 1000, //窗口宽度
+    frame: true, //是否显示窗口边框
+    resizable: false, //可否缩放
+    movable: true //可否移动
+  })
+
+  mainWindow.loadURL(winURL)
+
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
+
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
